5246829697c11f4102fba1c20a19a27e9c1835ca
Mohamed Ali <mohamedkhaledali@gmail.com>
2024-10-24 16:31:22 +0200

    Add project files.

---
+++ Quick BOM Compare/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quick_BOM_Compare
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static string GenerateFilePath(string fileName)
+        {
+            // Check if the file name ends with .dft .asm or .par or other.
+            if (fileName.EndsWith(".dft", StringComparison.OrdinalIgnoreCase))
+            {
+                return $"Z:\\Zeichnungen\\DFT\\{fileName}";
+            }
+            else if (fileName.EndsWith(".asm", StringComparison.OrdinalIgnoreCase))
+            {
+                return $"Z:\\Zeichnungen\\{fileName}";
+            }
+            else
+            {
+                return null;
+            }
+        }
+        static bool CheckIfPathExists(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string InputAsmName = TxtAsmName.Text;  // Get the Assembly name
+            string AsmFilePath = GenerateFilePath(InputAsmName); //Generate file path
+            bool pathExists = CheckIfPathExists(AsmFilePath);
+            if (pathExists == true)
+            {
+                LblPathStatus.Text = $"{AsmFilePath} ---> Exists :) ";
+            }
+            else if (pathExists == false)  
+            {
+                LblPathStatus.Text = $"{AsmFilePath} ---> Does not exist :(";
+            }
+        }
+
+    
+    }
+}
---
+++ Quick BOM Compare/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Quick BOM Compare")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Quick BOM Compare")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4aa97f40-5e2b-4b33-9f98-e8001e24d702")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
